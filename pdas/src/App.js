ba548eebfdc31ada86800ba6c3a363b51b056704
Leo <leolicheng@outlook.com>
2024-08-19 00:12:25 +1000

    lint the project

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,5 +1,5 @@
 import 'bulma/css/bulma.css';
-import ProfileCards from "./ProfileCards";
+import ProfileCards from './ProfileCards';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -10,21 +10,21 @@ function App() {
   // Fetch random users based on the number of cards
   const fetchUsers = async (num) => {
     if (num === 0) {
-        setUsers([]); // Clear the users array if input is 0
-        return;
-      }
+      setUsers([]); // Clear the users array if input is 0
+      return;
+    }
     try {
       const response = await axios.get(`https://randomuser.me/api/?results=${num}`);
       setUsers(response.data.results);
     } catch (error) {
-      console.error("Error fetching users:", error);
+      console.error('Error fetching users:', error);
     }
   };
 
   useEffect(() => {
     fetchUsers(numberOfCards);
   }, [numberOfCards]);
-  
+
   const handleInputChange = (e) => {
     const num = Math.min(Number(e.target.value), 10); // Cap the number of cards to 10
     setNumberOfCards(num);
@@ -35,13 +35,15 @@ function App() {
     <div>
       <section className="hero is-link">
         <div className="hero-body">
-            <p className="title">Assistants</p>
-            <p className="subtitle">Welcome to Virtual Assistants!</p>
+          <p className="title">Assistants</p>
+          <p className="subtitle">Welcome to Virtual Assistants!</p>
         </div>
       </section>
       <br />
       <div>
-        <label htmlFor="numCards" className="tag is-info is-large">Find your next incredible assistant(s): </label>
+        <label htmlFor="numCards" className="tag is-info is-large">
+          Find your next incredible assistant(s):
+        </label>
         <input
           className="input is-info"
           type="number"
@@ -52,27 +54,27 @@ function App() {
           max="10"
         />
       </div>
-      <div className="container"> 
+      <div className="container">
         <section className="section">
-            <div className="columns is-multiline">
+          <div className="columns is-multiline">
             {users.length > 0 ? (
-            users.map((user, index) => ( 
-              <div className="column is-half" key={index}>
-                <ProfileCards
-                  name={`${user.name.first} ${user.name.last}`}
-                  email={user.email}
-                  phone={user.phone}
-                  imageUrl={user.picture.large}
-                  cell={user.cell}
-                  description={user.location.timezone.description}
-                  id={user.id.name}
-                />
-              </div>
-            ))
-          ) : (
-            <p>No profile cards to display</p>
-          )}
-            </div>
+              users.map((user, index) => (
+                <div className="column is-half" key={index}>
+                  <ProfileCards
+                    name={`${user.name.first} ${user.name.last}`}
+                    email={user.email}
+                    phone={user.phone}
+                    imageUrl={user.picture.large}
+                    cell={user.cell}
+                    description={user.location.timezone.description}
+                    id={user.id.name}
+                  />
+                </div>
+              ))
+            ) : (
+              <p>No profile cards to display</p>
+            )}
+          </div>
         </section>
       </div>
     </div>
